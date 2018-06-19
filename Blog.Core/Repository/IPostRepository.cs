c51734abdbb9019c298cc9e3711631a6b1cddad6
Rafal Szopa <rafals_92@tlen.pl>
2018-06-19 23:18:32 +0200

    Added very first version of repository interfaces to Blog.Core projects

---
+++ Blog.Core/Repository/IPostRepository.cs
@@ -0,0 +1,20 @@
+﻿using Blog.Core.Models;
+using System.Collections.Generic;
+
+namespace Blog.Core.Repository
+{
+    public interface IPostRepository
+    {
+        int Add(Post entity);
+
+        Post GetById(int id);
+
+        void Delete(int id);
+
+        void Update(Post entity);
+
+        Post GetFullPostById(int id);
+
+        IEnumerable<Post> GetAll();
+    }
+}
---
+++ Blog.Core/Repository/ITagRepository.cs
@@ -0,0 +1,20 @@
+﻿using Blog.Core.Models;
+using System.Collections.Generic;
+
+namespace Blog.Core.Repository
+{
+    public interface ITagRepository
+    {
+        int Add(Tag tag);
+
+        void AddMany(IEnumerable<Tag> tags);
+
+        int DeleteByName(string tagName);
+
+        Tag GetByName(string tagName);
+
+        IEnumerable<Tag> GetTagsByPostId(int postId);
+
+        IEnumerable<Tag> GetAll();
+    }
+}
