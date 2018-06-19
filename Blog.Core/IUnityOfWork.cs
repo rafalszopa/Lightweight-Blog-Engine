23632685b07b04f433aae2593391433494d7dd02
Rafal Szopa <rafals_92@tlen.pl>
2018-06-20 00:28:21 +0200

    Added very first outline of UnityOfWork

---
+++ Blog.Core/IUnityOfWork.cs
@@ -0,0 +1,15 @@
+﻿using Blog.Core.Repository;
+
+namespace Blog.Core
+{
+    public interface IUnityOfWork
+    {
+        IPostRepository PostRepository { get; }
+
+        ITagRepository TagRepository { get; }
+
+        IUserRepository UserRepository { get; }
+
+        void Commit();
+    }
+}
---
+++ Blog.Persistance/UnityOfWork.cs
@@ -0,0 +1,71 @@
+﻿using Blog.Core;
+using System;
+using System.Data;
+using Blog.Core.Repository;
+
+namespace Blog.Persistance
+{
+    /* TO DO:
+     - pass connection string from constructor to ConnectionFactory
+     - think about DI */
+    public class UnityOfWork : IUnityOfWork, IDisposable
+    {
+        #region private fields
+
+        private IDbConnection connection;
+
+        private IDbTransaction transaction;
+
+        private IPostRepository postRepository;
+
+        private ITagRepository tagRepository;
+
+        private IUserRepository userRepository;
+
+        #endregion
+
+        #region Constructor
+
+        public UnityOfWork(string connection)
+        {
+            this.connection = ConnectionFactory.Get;
+            this.transaction = this.connection.BeginTransaction();
+        }
+
+        #endregion
+
+        public IPostRepository PostRepository
+        {
+            get
+            {
+                return this.postRepository ?? (this.postRepository = new PostRepository(this.transaction));
+            }
+        }
+
+        public ITagRepository TagRepository
+        {
+            get
+            {
+                return this.tagRepository ?? (this.tagRepository = new TagRepository(this.transaction)); ;
+            }
+        }
+
+        public IUserRepository UserRepository
+        {
+            get
+            {
+                return this.userRepository ?? (this.userRepository = new UserRepository(this.transaction)); ;
+            }
+        }
+
+        public void Commit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
