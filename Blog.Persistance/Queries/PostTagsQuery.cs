af904b5e30e37abc85ca678dcd41d622c47e88ce
Rafal Szopa <rafals_92@tlen.pl>
2018-06-20 23:47:45 +0200

    Renamed 'PostTagQuery' to 'PostTagsQuery'

---
+++ Blog.Persistance/Queries/PostTagsQuery.cs
@@ -0,0 +1,15 @@
+﻿namespace Blog.Persistance.Queries
+{
+    internal static class PostTagsQuery
+    {
+        public static string Add()
+        {
+            return @"INSERT INTO Post_Tag(PostId, TagId) VALUES(@PostId, @TagId)";
+        }
+
+        public static string Delete()
+        {
+            return "DELETE FROM Post_Tag WHERE PostId = @PostId AND TagId = @TagId";
+        }
+    }
+}
