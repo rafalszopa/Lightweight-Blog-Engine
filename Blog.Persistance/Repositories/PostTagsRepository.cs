8e03a6b82b738e079184c150172472f5c9cb79b8
Rafal Szopa <rafals_92@tlen.pl>
2018-06-20 23:15:33 +0200

    Added base PosTagsRepository class

---
+++ Blog.Persistance/Repositories/PostTagsRepository.cs
@@ -0,0 +1,37 @@
+﻿using Blog.Core.Models;
+using Dapper;
+using System.Collections.Generic;
+using System.Data;
+
+namespace Blog.Persistance
+{
+    public class PostTagsRepository
+    {
+        private IDbTransaction transaction;
+
+        private IDbConnection connection { get { return transaction.Connection; } }
+
+        public PostTagsRepository(IDbTransaction transaction)
+        {
+            this.transaction = transaction;
+        }
+
+        public void Add(int postId, Tag tag)
+        {
+            this.connection.Execute(@"INSERT INTO Post_Tag(PostId, TagId) VALUES(@PostId, @TagId)", new { PostId = postId, TagId = tag.Id }, this.transaction);
+        }
+
+        public void AddTags(int postId, IEnumerable<Tag> tags)
+        {
+            string query = @"INSERT INTO Post_Tag(PostId, TagId)";
+
+            foreach(var tag in tags)
+            {
+                query = string.Format(@"VALUES({0}, {1});", postId, tag.Id);
+            }
+            query += ";";
+
+            this.connection.Execute(query, this.transaction);
+        }
+    }
+}
