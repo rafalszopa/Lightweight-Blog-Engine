348f52cac737cf2fc88a15d28dd8cdcb079f540f
Rafal Szopa <rafals_92@tlen.pl>
2018-06-23 01:00:43 +0200

    Implement initial version of HomePage service

---
+++ Blog.MVC/Services/HomePageServices.cs
@@ -0,0 +1,38 @@
+﻿using Blog.Core;
+using Blog.Core.Models;
+using Blog.Persistance;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Blog.MVC.Services
+{
+    public class HomePageServices : IHomePageServices
+    {
+        IUnityOfWork unitOfWork;
+
+        public HomePageServices()
+        {
+            /*his.unitOfWork = new UnityOfWork("connectionString");*/
+        }
+
+        public IEnumerable<Post> GetPosts()
+        {
+            using (var unitOfWork = new UnityOfWork("connectionString"))
+            {
+                var posts = unitOfWork.PostRepository.GetAll();
+
+                foreach(var post in posts)
+                {
+                    post.Tags = unitOfWork.TagRepository.GetTagsByPostId(post.Id).ToList();
+                }
+
+                return posts;
+            }
+        }
+    }
+
+    public class AppSettings
+    {
+        public int NumberOfHomePosts = 5;
+    }
+}
---
+++ Blog.MVC/Services/IHomePageServices.cs
@@ -0,0 +1,10 @@
+﻿using Blog.Core.Models;
+using System.Collections.Generic;
+
+namespace Blog.MVC.Services
+{
+    public interface IHomePageServices
+    {
+        IEnumerable<Post> GetPosts();
+    }
+}
