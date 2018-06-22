ecf9e2b6ce9e26e58f7d25db581641787eeb90fd
Rafal Szopa <rafals_92@tlen.pl>
2018-06-23 01:02:40 +0200

    Amendment of last commit (added missing files)

--- Blog.MVC/Controllers/HomeController.cs
+++ Blog.MVC/Controllers/HomeController.cs
@@ -5,17 +5,32 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Blog.MVC.Models;
+using Blog.MVC.Services;
 
 namespace Blog.MVC.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IHomePageServices homePageService;
+
+        public HomeController()
+        {
+            this.homePageService = new HomePageServices();
+        }
+
         public IActionResult Index()
+        {
+            var posts = this.homePageService.GetPosts();
+
+            return View(posts);
+        }
+
+        public IActionResult About()
         {
             return View();
         }
 
-        public IActionResult About()
+        public IActionResult Posts()
         {
             return View();
         }
