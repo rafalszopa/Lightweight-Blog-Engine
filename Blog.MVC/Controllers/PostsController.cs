2b7af26e6e8673ab85a5b10710cfa37440428fe2
Rafal Szopa <rafals_92@tlen.pl>
2018-06-25 23:20:17 +0200

    Added PostsController

---
+++ Blog.MVC/Controllers/PostsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Blog.MVC.Models;
+using Blog.MVC.Services;
+
+namespace Blog.MVC.Controllers
+{
+    public class PostsController : Controller
+    {
+        private readonly HomePageServices homePageService;
+
+        public PostsController()
+        {
+            this.homePageService = new HomePageServices();
+        }
+
+        public IActionResult Index()
+        {
+            var posts = this.homePageService.GetPosts();
+
+            return View(posts);
+        }
+    }
+}
