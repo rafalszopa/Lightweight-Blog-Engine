4c0e69153a5df5b8187f6d42e1f3ad440a6f87fa
Rafal Szopa <rafals_92@tlen.pl>
2018-07-07 18:54:48 +0200

    Added UserFilter class

---
+++ Blog.Core/Repository/Filters/UserFilter.cs
@@ -0,0 +1,24 @@
+﻿namespace Blog.Core.Repository.Filters
+{
+    public class UserFilter
+    {
+        public IntegerColumn Id { get; private set; }
+
+        public TextColumn FirstName { get; private set; }
+
+        public TextColumn LastName { get; private set; }
+
+        public IntegerColumn Type { get; private set; }
+
+        public DateColumn Create { get; private set; }
+
+        public UserFilter()
+        {
+            this.Id = new IntegerColumn("Id");
+            this.FirstName = new TextColumn("FirstName");
+            this.LastName = new TextColumn("LastName");
+            this.Type = new IntegerColumn("UserTypeId");
+            this.Create = new DateColumn("CreateDate");
+        }
+    }
+}
