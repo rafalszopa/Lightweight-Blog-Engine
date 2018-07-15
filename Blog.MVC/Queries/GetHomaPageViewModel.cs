914d8ea767436ab583beac7fbe0559ad953570bb
Rafal Szopa <rafals_92@tlen.pl>
2018-07-15 22:35:08 +0200

    Applied Query pattern to MVC project

---
+++ Blog.MVC/Queries/GetHomaPageViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Blog.MVC.ViewModels;
+using System.Collections.Generic;
+using System.Data;
+using Dapper;
+
+namespace Blog.MVC.Queries
+{
+    public class GetHomaPageViewModel : IQuery<IEnumerable<HomePageViewModel>>
+    {
+        public IEnumerable<HomePageViewModel> Execute(IDbConnection connection)
+        {
+            var query =
+                @"SELECT * FROM HomePageView
+                ORDER BY PublishDate
+                OFFSET @PageIndex * @PageSize ROWS FETCH NEXT @PageSize ROWS ONLY";
+
+            var result = connection.Query<dynamic>(query, new { PageIndex = 0, PageSize = 5 });
+            return Slapper.AutoMapper.MapDynamic<HomePageViewModel>(result);
+        }
+    }
+}
---
+++ Blog.MVC/Queries/IQuery.cs
@@ -0,0 +1,9 @@
+﻿using System.Data;
+
+namespace Blog.MVC.Queries
+{
+    public interface IQuery<T> where T : class
+    {
+        T Execute(IDbConnection connection);
+    }
+}
