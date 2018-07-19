802a319884173193f35ba578ed4c1718aa1aebed
Rafal Szopa <rafals_92@tlen.pl>
2018-07-19 22:44:40 +0200

    Created dummy components

--- Blog.API/ClientApp/routes.js
+++ Blog.API/ClientApp/routes.js
@@ -1,9 +1,15 @@
 import CounterExample from 'components/counter-example'
 import FetchData from 'components/fetch-data'
 import HomePage from 'components/home-page'
+import Posts from 'components/posts'
+import Media from 'components/media'
+import Tags from 'components/tags'
 
 export const routes = [
     { path: '/', component: HomePage, display: 'Home', style: 'glyphicon glyphicon-home' },
+    { path: '/posts', component: Posts, display: 'Posts', style: 'glyphicon glyphicon-th-list' },
+    { path: '/media', component: Media, display: 'Media', style: 'glyphicon glyphicon-th-list' },
+    { path: '/tags', component: Tags, display: 'Tags', style: 'glyphicon glyphicon-th-list' },
     { path: '/counter', component: CounterExample, display: 'Counter', style: 'glyphicon glyphicon-education' },
-    { path: '/fetch-data', component: FetchData, display: 'Fetch data', style: 'glyphicon glyphicon-th-list' }
+    { path: '/fetch-data', component: FetchData, display: 'Fetch data', style: 'glyphicon glyphicon-th-list' },
 ]
